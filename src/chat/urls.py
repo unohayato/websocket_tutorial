84af5fbab487d083ea8a35360f877754ee1f03c4
unohayato <mamidori1p1p@gmail.com>
2024-02-29 23:35:02 +0900

    :fire:

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -3,4 +3,5 @@ from chat import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
--- src/config/asgi.py
+++ src/config/asgi.py
@@ -9,14 +9,23 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+django_asgi_app = get_asgi_application()
+
+import chat.routing
+
 # プロトコルごとにルーティングを切り替えるための記述
 application = ProtocolTypeRouter(
   {
-    "http": get_asgi_application(), # http->非同期モード
+    "http": django_asgi_app, # http->非同期モード
+    "websocket": AllowedHostsOriginValidator(
+      AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+    ),
   }
 )

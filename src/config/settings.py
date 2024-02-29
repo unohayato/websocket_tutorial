c55e9c494c86e7daeec9fcaf9189aba4423d0d51
unohayato <mamidori1p1p@gmail.com>
2024-02-29 22:10:33 +0900

    :sparkles: add daphne and channels

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,13 +37,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne'
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat',
 ]
 
 MIDDLEWARE = [
